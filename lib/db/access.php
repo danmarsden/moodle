577b79e114c57f0edc8990262b86ebbe23bfdd14
Dan Marsden <dan@danmarsden.com>
2020-04-29 08:19:40 +1200

    MDL-68054 lib: viewhiddenactivities should be set as captype:read

--- lib/db/access.php
+++ lib/db/access.php
@@ -961,7 +961,7 @@ $capabilities = array(
 
     'moodle/course:viewhiddenactivities' => array(
 
-        'captype' => 'write',
+        'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => array(
             'teacher' => CAP_ALLOW,
@@ -1550,7 +1550,7 @@ $capabilities = array(
 
     'moodle/course:viewhiddensections' => array(
 
-        'captype' => 'write',
+        'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
